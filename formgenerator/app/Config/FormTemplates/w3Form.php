879a403a3631fdd08c4472dd8eadb236c5bf9df4
skyish242 <atap2749@gmail.com>
2023-06-23 14:34:01 +0800

    heh

---
+++ formgenerator/app/Config/FormTemplates/w3Form.php
@@ -0,0 +1,73 @@
+<?php
+
+
+// Get an instance of the FormBuilder2 library
+$formBuilder = service('formBuilder2');
+
+$fields = [
+    'name' => [
+        //new_div($label, $input, $row, $span, $column, $attributes)
+        'group' => $formBuilder->new_div(
+            array(
+                //new_label($name='', $value='', $attributes='')
+                $formBuilder->new_label('name', 'Name'), 
+                //new_input($name='', $value='', $attributes='' OR $attributes=[])
+                $formBuilder->new_input('name', '', 'class="form-control" id="name-control" placeholder="Enter your name" required'), 
+            ),
+            1,'md', 9, 'mt-5'
+        )        
+    ],
+    'message' => [
+        'group' => $formBuilder->new_div(
+            array(
+                //new_label($name='', $value='', $attributes='')
+                $formBuilder->new_label('message', 'Message', 'class="message-label-control"'),
+                //new_textarea($name='', $value='', $attributes='' OR $attributes=[])
+                $formBuilder->new_textarea(
+                    'message', '',
+                    array('class'=> 'form-control message-control', 'placeholder'=>'Enter you message', 'required'=> true)
+                ),
+            ),
+            2,'md', 9
+        )
+    ],
+    'sex-help' => [
+        'group' => $formBuilder->new_div(
+            [
+                $formBuilder->new_div(
+                    [
+                        $formBuilder->new_label('sex-help', 'How to sex'),
+                        $formBuilder->new_para_helper('sex-help', 'Key in which sex group you are', 'id="sex-helper"'),
+                    ],
+                    '', 'md', 2
+                ),
+                $formBuilder->new_div(
+                    [
+                        $formBuilder->new_label('gender', 'Choose Gender:'),
+                        '<br>',
+                        $formBuilder->new_div(
+                            [
+                                $formBuilder->new_radio('gender', 'male', 'id="male" class="form-check-input"'),
+                                $formBuilder->new_label('male', 'Male', 'class="form-check-label"'),
+                            ],
+                            '', '', '', 'form-check form-check-inline'
+                        ),
+                        $formBuilder->new_div(
+                            [
+                                $formBuilder->new_radio('gender', 'female', 'id="female" class="form-check-input"'),
+                                $formBuilder->new_label('female', 'Female', 'class="form-check-label"'),
+                            ],
+                            '', '', '', 'form-check form-check-inline'
+                        ),
+                    ],
+                    '', 'md', '5', 'radio-control'
+                ),
+            ],
+            'row'
+        )
+    ],
+
+    
+];
+
+return $fields;
---
+++ formgenerator/app/Controllers/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+use Exception;
+
+class AdminController extends BaseController
+{
+    public function index()
+    {
+        //Get the library instance
+        $formBuilder = service('formBuilder2');
+        //Get the Form Fields
+        $fields = include(APPPATH . 'Config/FormTemplates/w3Form.php');
+        $data  = [
+            'Name' => "Meow Meow Chart",
+            'Structure' => $fields
+        ];
+
+        /*
+            Possible Parameters for $data , else default
+            Complusory is 'Structure'!
+            $data  = [
+                'Name' => 
+                'Version' =>
+                'Datetime' =>
+                'Description' =>
+                'Structure' => 
+            ];
+        */
+
+        //Call the library to insert the form template 
+        try{
+            $formID = $formBuilder->newFormTemplate($data);
+
+            $data = [
+                'title' => 'Successful Insertion',
+                'formID'  => $formID
+            ];
+
+            return view('templates/header', $data)
+            . view('success')
+            . view('templates/footer');
+
+        }catch(Exception $e){
+            // Show the default CodeIgniter error page with the error message
+            show_error('An error occurred while inserting the form data. Please try again.');
+        }
+    }
+}
